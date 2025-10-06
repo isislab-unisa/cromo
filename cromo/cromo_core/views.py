6cdb06fbc5c26180fd4071fb70ccd2f692f66933
DarioDeMaio <demaio.dario95@gmail.com>
2025-10-06 17:37:10 +0200

    fix: nginx.conf, views.py

--- cromo/cromo_core/views.py
+++ cromo/cromo_core/views.py
@@ -549,7 +549,7 @@ def proxy_id_cromopoi(request):
     }
 
     try:
-        r = requests.get(url, headers=headers, timeout=30)
+        r = requests.get(url, headers=headers, timeout=10)
     except Exception as e:
         return JsonResponse({"error": str(e)}, status=500)
 
